ca167af20cfaeb24440454648889f5bbefbf9878
chrisramfon <chrisramfon@outlook.com>
2020-11-01 17:53:56 -0600

    Implementación de diseño
    Se agregan los archivos del diseño, se crean los componentes "inicio", "footer" y menu.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'inicio', component:InicioComponent},
+  //Rutas por defecto, dejar al final
+  {path:'',redirectTo:'inicio', pathMatch:'full'},
+  {path:'**', redirectTo:'inicio',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
