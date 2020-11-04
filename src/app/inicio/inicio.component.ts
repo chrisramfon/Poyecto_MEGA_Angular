824c973fce08231ac28a110ffe514f1dbb44c2a9
chrisramfon <chrisramfon@outlook.com>
2020-11-04 00:41:44 -0600

    Correccion de warning
    Se corrige el warning que hace referencia a la libreria owl-carousel

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -8,7 +8,6 @@ import {Router} from '@angular/router';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
-<<<<<<< HEAD
 
   usuario={
     usuario:"",
@@ -16,14 +15,12 @@ export class InicioComponent implements OnInit {
   }
 
   constructor(private iniciosesion:IniciosesionService, private router:Router) { }
-=======
   title = 'owlcarouselinAngular';  
   Images = ['../assets/images/Carousel1.jpeg', '../assets/images/Carousel2.jpeg', '../assets/images/Carousel3.jpeg'];  
   
   SlideOptions = { items: 1, dots: true, nav: true };  
   CarouselOptions = { items: 3, dots: true, nav: true };  
-  constructor() { }
->>>>>>> 8726b8940a9ebbddcc7491d6fe0d681b1c900b2d
+  
 
   ngOnInit(): void {
   }
